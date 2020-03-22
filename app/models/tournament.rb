4d203522f14fafb57013e025b98c4dd38f66d1ae
kaoru-gumi-company <kaoru_gumi_company@icloud.com>
2020-03-22 13:34:09 +0900

    Create tournament model.

---
+++ app/models/tournament.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Tournament < ApplicationRecord
+  validates :name,       presence: true
+  validates :game_type,  presence: true
+  validates :prize,      presence: true, numericality: { only_integer: true }
+  validates :evented_at, presence: true
+
+  enum game_type: {
+    single: 1,
+    duo: 2,
+    squad: 4,
+    quintetto: 5
+  }
+end
