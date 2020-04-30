943bb8c7baaccbc5b05108097bc6e6ea8f268665
kaoru-gumi-company <kaoru_gumi_company@icloud.com>
2020-04-30 12:55:45 +0900

    バリデーションエラー時にfield_with_errorsクラスを付与しないようにする

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@
 require_relative 'boot'
 
 require 'rails'
-# Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_job/railtie'
 require 'active_record/railtie'
@@ -15,10 +14,7 @@ require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
 require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module AvalonNet
@@ -29,13 +25,10 @@ module AvalonNet
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, _instance| html_tag }
+
     config.generators do |g|
       g.system_tests = nil
       g.assets false
